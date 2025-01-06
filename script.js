1f3de266a914c98cf161a852cabf2fe1eac6ffa6
Pemzylee <pemzylee123@gmail.com>
2025-01-06 18:06:22 +0100

    sec commit

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const yesButton = document.getElementById("yesBtn");
+const noButton = document.getElementById("noBtn");
 const modal = document.getElementById("modal");
 const Maincontainer = document.getElementById("mainModal");
 const closeModal = document.getElementById("closeBtn");
 const sentButton = document.getElementById("sentBtn");
+const copyButton = document.getElementById("copyBtn")
+
+// Random movement for the "No" button
+noButton.addEventListener("mouseover", () => {
+  const randomTop = Math.random() * 80 + 10;
+  const randomLeft = Math.random() * 80 + 10;
+
+  noButton.style.position = "absolute";
+  noButton.style.top = `${randomTop}%`;
+  noButton.style.left = `${randomLeft}%`;
+});
 
 // Open modal on "Yes" button click
 yesButton.addEventListener("click", () => {
@@ -19,4 +31,11 @@ closeModal.addEventListener("click", () => {
 sentButton.addEventListener("click", () => {
   modal.classList.remove("active");
   Maincontainer.style.display = 'block';
+});
+
+copyButton.addEventListener("click", () => {
+  const accountDetails = `
+    7011750735
+  `;
+  navigator.clipboard.writeText(accountDetails)
 });
